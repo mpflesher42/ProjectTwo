876208ed4a4ff38fd0a4d7b2d8e0da20d6186d7d
David-codey <dfields42@tntech.edu>
2024-10-08 16:00:19 -0500

    Added in the functions and stuff

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -1,8 +1,30 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LINK_H
+#include <string>
+#include <list>
+#include<fstream>
+using std::string;
+class LinkedList{
+    private:
+    LinkedList *head;
+    LinkedList *tail;
+    string word;
+    int index;
+    public:
+        LinkedList getHeadPointer(LinkedList* headPtr){
+            return *headPtr;
+        }
+        LinkedList getTailPointer(LinkedList* tailPtr){
+            return *tailPtr;
+        }
+        void setHeadPointer(LinkedList*);
+        void setTailPointer(LinkedList*);
+        string appendList(string);
+        string prependList(string);
+        string insertList(string);
+        int removeFromList(string, int);
+        friend int sortingFunction(int);
+        friend ostream operator<< (ostream& const LinkedList&);
 
-struct ListNode{
-    struct ListNode *next;
-    struct ListNode *previous;
-}
-#endif;
+};
+#endif
