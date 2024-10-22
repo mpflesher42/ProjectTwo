f625a4a5a5de3066528fe912c5be819b2a02930f
David-codey <dfields42@tntech.edu>
2024-10-22 15:51:34 -0500

    I like the prospect of adding comments to a commit. I don't like HAVING to commment to commit.

--- storage.hpp
+++ storage.hpp
@@ -1,12 +1,26 @@
 #ifndef STORAGE_H
 #define STORAGE_H
+#include<string>
 template<typename typeShit>
-class SmartPointer
+class storage
 {
 private:
-    typeShit* thingNode;
-public:
+    struct ListNode
+    {
+        ListNode* next;
+        ListNode* last;
+        string countyName;
+        int population;
+    };
+    ListNode* head;
+    ListNode* tail;
     
+public:
+
+    storage::storage(){// default constructor
+        head = NULL;
+        tail = NULL;
+    }
 };
 
 #endif
