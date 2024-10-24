46db07b5f9bf715ed4d064ef5cdae22cb9a949ad
David-codey <dfields42@tntech.edu>
2024-10-24 13:43:22 -0500

    Added in the functions from linkedlist.hpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,1 +1,20 @@
 #include "linkedlist.hpp"
+        
+void appendList(string thingToAppend){
+
+}
+void prependList(string thingToPrepend){
+
+}
+void insertList(string thingToInsert){
+
+}
+void removeFromList(string thingToRemove, int index){
+
+}
+int sortingFunction(int key){
+
+}
+LinkedList:: friend ostream& operator<<(ostream& output, const LinkedList& Ll){
+    output << 
+}
