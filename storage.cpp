379d510d1c0291bc07c00bf357564540270a07c2
Noah Christopher Carter <nccarter42@tntech.edu>
2024-10-24 15:51:52 -0500

    Add TO DO list

--- storage.cpp
+++ storage.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 void save()
 {
-    
+    // TO DO: Implement save to "counties.txt" function
 }
 
 void load()
 {
-
+    // TO DO: Implement load from "counties.txt" function
 }
