208c60432eaa26441ec7ee3e64bbe1ea2c6e3d91
Mal Flesher <mpflesher42@tntech.edu>
2024-10-29 15:48:32 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -21,10 +21,44 @@ int main() {
     //user menu options
     do {
         cout << "What would you like to do with your county?" << endl; //add in options
+        //LinkedList<typename> variableName;
 
         switch (choice) {
             case 1:
-                //call other case 1 functions and get other needed prompts from user
+                //getNext
+                break;
+            case 2:
+                //getLast
+                break;
+            case 3:
+                //setNext
+                break;
+            case 4:
+                //setLast
+                break;
+            case 5:
+                //appendIntoList
+                break;
+            case 6:
+                //prependIntoList
+                break;
+            case 7:
+                //insertIntoList
+                break;
+            case 8:
+                //removeFromList
+                break;
+            case 9:
+                //swapPositions
+                break;
+            case 10:
+                //sortingFunction
+                break;
+            case 11:
+                //save
+                break;
+            case 12:
+                //load
                 break;
             default:
                 cout << "\nThat was not a valid choice. Please choose a number between 1-6:\t" << endl;;
@@ -32,7 +66,6 @@ int main() {
         }
 
     //change numeber based on needed menu options
-    // LinkedList<typename whatever> variableName;
     } while (choice != 6);
 
     return 0;
