9652cbd0e25cb2cc85dd2c7bf09744988ce99481
David-codey <dfields42@tntech.edu>
2024-10-31 15:08:14 -0500

    Fixed comment

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -1,6 +1,7 @@
 /*File Name LinkedList.hpp
 Author: David Fields
-Purpose: Stores the prototypes for LinkedList.cpp*/
+Purpose: Stores the prototypes for LinkedList.cpp
+*/
 
 #ifndef LINKED_LIST_H
 #define LINKED_LINK_H
