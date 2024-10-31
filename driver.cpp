617e5a8f9479b3bd48a5f8f9ed0e6c26bac8b523
Mal Flesher <mpflesher42@tntech.edu>
2024-10-31 14:25:52 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -2,8 +2,7 @@
     File Name: Driver.cpp
     Author Name: Mal Flesher
     Date: 11/1/2024
-    Purpose: A program for storing and sorting
-             through fantasy counties
+    Purpose: A program for storing and sorting through fantasy counties
 */
 
 #include "county.hpp"
@@ -22,10 +21,10 @@ int main() {
     //user menu options
     do {
         cout << "What would you like to do with your county?" << endl; //add in options
-        cout << "1.\tAppend to list" << endl;
-        cout << "2.\tPrepend to list" << endl;
-        cout << "3.\tAdd to list." << endl;
-        cout << "4.\tRemove from list" << endl;
+        cout << "1.\tAdd to end of list" << endl;
+        cout << "2.\tAdd to front of list" << endl;
+        cout << "3.\tAdd to anywhere in list" << endl;
+        cout << "4.\tRemove from end of list list" << endl;
         cout << "5.\tSort list" << endl;
         cout << "6.\tSave list" << endl;
         cout << "7.\tLoad previous list" << endl;
@@ -47,18 +46,23 @@ int main() {
         switch (choice) {
             case 1:
                 //appendIntoList
+                LinkedList<something> apppendIntoList();
                 break;
             case 2:
                 //prependIntoList
+                LinkedList<something> prependIntoList();
                 break;
             case 3:
                 //insertIntoList
+                LinkedList<something> insertIntoList();
                 break;
             case 4:
                 //removeFromList
+                LinkedList<something> removeFromList();
                 break;
             case 5:
-                //sortingFunction
+                //sorting Function
+                LinkedList<something> sortingFunction();
                 break;
             case 6:
                 //save
