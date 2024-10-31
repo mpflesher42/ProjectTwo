68471da2526bd1a6e59744fb30fc7d8fd2ffabb6
Mal Flesher <mpflesher42@tntech.edu>
2024-10-31 11:26:24 -0500

    updating driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -2,7 +2,8 @@
     File Name: Driver.cpp
     Author Name: Mal Flesher
     Date: 11/1/2024
-    Purpose: 
+    Purpose: A program for storing and sorting
+             through fantasy counties
 */
 
 #include "county.hpp"
@@ -21,43 +22,48 @@ int main() {
     //user menu options
     do {
         cout << "What would you like to do with your county?" << endl; //add in options
+        cout << "1.\tAppend to list" << endl;
+        cout << "2.\tPrepend to list" << endl;
+        cout << "3.\tAdd to list." << endl;
+        cout << "4.\tRemove from list" << endl;
+        cout << "5.\tSort list by " << endl;
+        cout << "6.\tSave list" << endl;
+        cout << "7.\tLoad previous list" << endl;
+        cout << "8.\tEnd Program" << endl;
+        cout << "Choose 1-8:\t";
+        cin >> choice;
+
+        //validating user input
+        if (choice < 1 || choice > 8) {
+            cout << "\n\tInvaild Choice" << endl;
+            cout << "\tChoose 1-11:\t";
+            cin >> choice;
+        }
+        cin.ignore();
+        
+        
         //LinkedList<typename whatever> variableName;
 
         switch (choice) {
             case 1:
-                //getNext
-                break;
-            case 2:
-                //getLast
-                break;
-            case 3:
-                //setNext
-                break;
-            case 4:
-                //setLast
-                break;
-            case 5:
                 //appendIntoList
                 break;
-            case 6:
+            case 2:
                 //prependIntoList
                 break;
-            case 7:
+            case 3:
                 //insertIntoList
                 break;
-            case 8:
+            case 4:
                 //removeFromList
                 break;
-            case 9:
-                //swapPositions
-                break;
-            case 10:
+            case 5:
                 //sortingFunction
                 break;
-            case 11:
+            case 6:
                 //save
                 break;
-            case 12:
+            case 7:
                 //load
                 break;
             default:
@@ -66,7 +72,7 @@ int main() {
         }
 
     //change numeber based on needed menu options
-    } while (choice != 6);
+    } while (choice != 8);
 
     return 0;
 }
