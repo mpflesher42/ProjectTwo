b89c0d2e31729af00d88dac746320a48d155724a
David-codey <dfields42@tntech.edu>
2024-11-01 10:35:49 -0500

    Something

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -11,7 +11,7 @@ Purpose: Stores the prototypes for LinkedList.cpp
 #include <iostream>
 #include "county.hpp"
 using namespace std;
-template<class something>
+template<typename something>
 class LinkedList{
     private:
     struct ListNode
